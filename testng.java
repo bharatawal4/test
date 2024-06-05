c165298d01af0e4df51ea63c084b830bb3cba78b
bharatawal4 <163956860+bharatawal4@users.noreply.github.com>
2024-06-05 16:11:28 +0545

    Add files via upload

---
+++ testng.java
@@ -0,0 +1,25 @@
+package TestNG;
+
+import org.testng.annotations.Test;
+
+public class testng {
+	
+	@Test
+	public void login() {
+		System.out.println("this is login");
+	}
+	
+public void logout() {
+		System.out.println("this is logout");
+	}
+public void signin() {
+		System.out.println("this is signin");
+	}
+
+
+public void signout() {
+		System.out.println("this is signout");
+	}
+
+
+}
